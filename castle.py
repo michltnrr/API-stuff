c7effea8e109deeeaff64fd7bd4df83fb9803c29
Michael Turner <70729675+michltnrr@users.noreply.github.com>
2024-02-05 21:44:53 -0500

    Update castle.py

--- castle.py
+++ castle.py
@@ -2,13 +2,11 @@ import requests
 import json
 params = {'User-Agent': 'yourchess.com email'}
 response = requests.get('https://api.chess.com/pub/player/{personsusername}', headers=params)
-# print(response.status_code)
+
 user_data = (response.json())
-# print(user_data)
 print(user_data['username'], user_data['country'][-2:])
 response = requests.get('https://api.chess.com/pub/player/{personsusername}/stats', headers=params)
 user_stats = response.json()
-# print(user_stats)
-# print(user_stats['chess_blitz'])
+
 print(user_stats['chess_blitz']['last']['rating'])
 print(user_stats['chess_blitz']['record'])
